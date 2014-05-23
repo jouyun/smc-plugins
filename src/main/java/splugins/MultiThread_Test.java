1703ff74ddeaadf636eafd632169a0ee8877b4a1
Sean McKinney <smc@newton.sgc.loc>
2014-05-23 11:21:17 -0500

    Multithread

---
+++ src/main/java/splugins/MultiThread_Test.java
@@ -0,0 +1,125 @@
+package splugins;
+
+import ij.*;  
+import ij.plugin.PlugIn;  
+import ij.process.*;  
+import ij.io.Opener;  
+import imagescience.feature.Differentiator;
+import imagescience.image.Aspects;
+import imagescience.image.Dimensions;
+import imagescience.image.FloatImage;
+import imagescience.image.Image;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultiThread_Test implements PlugIn {
+
+	@Override
+
+	public void run(String arg) {  
+	  
+		final ImagePlus dot_blot = new Opener()  
+			.openURL("http://rsb.info.nih.gov/ij/images/blobs.gif");  
+		
+		
+		
+
+		final int starting_threshold = 1;  
+		final int ending_threshold = 15;  
+		final int n_tests = ending_threshold - starting_threshold + 1;  
+		final AtomicInteger ai = new AtomicInteger(starting_threshold);  
+
+		// store all result images here  
+		final ImageProcessor[] results = new ImageProcessor[n_tests];  
+
+		final Thread[] threads = newThreadArray(); 
+		
+		final int [] scale_array={2, 4, 8, 16, 32, 2, 4, 8, 16, 32, 2, 4, 8, 16, 32};
+		final int [] order_array={2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4};
+		
+		/*for (int ithread = 0; ithread < threads.length; ithread++) 
+		{  
+
+			// Concurrently run in as many threads as CPUs  
+
+			threads[ithread] = new Thread() 
+			{  
+                      
+				{ setPriority(Thread.NORM_PRIORITY); }  
+
+				public void run() 
+				{  
+
+					// Each thread processes a few items in the total list  
+					// Each loop iteration within the run method  
+					// has a unique 'i' number to work with  
+					// and to use as index in the results array:  
+
+					for (int i = ai.getAndIncrement(); i <= ending_threshold;  
+							i = ai.getAndIncrement()) 
+					{  
+						// 'i' is the lower bound of the threshold window  
+						ImageProcessor ip = dot_blot.getProcessor().duplicate();
+						//ip.setInterpolationMethod(2);
+						//ip.rotate((double)i/10.0);
+						//(new FJLaplacian()).run()
+						//ip.setMinAndMax(i, 255);  
+						ImagePlus imp = new ImagePlus("Threshold " + i, ip);
+						
+						final Image img = Image.wrap(imp);
+						final Image newimg = new FloatImage(img);
+						final Differentiator diff = new Differentiator();
+						diff.run(newimg,scale_array[i],order_array[i],order_array[i],0);
+						//newimg.imageplus().show();
+						results[i-starting_threshold] = newimg.imageplus().getProcessor();  
+						//res.getWindow().setVisible(false);  
+					}  
+				}
+			};  
+		}  
+
+    	startAndJoin(threads);  
+
+    	// 	now the results array is full. Just show them in a stack:  
+    	final ImageStack stack = new ImageStack(dot_blot.getWidth(),  
+                                            	dot_blot.getHeight());  
+    	for (int i=0; i< results.length; i++) {  
+    		stack.addSlice(Integer.toString(i), results[i]);  
+    	}  
+
+    	new ImagePlus("Results", stack).show();  */
+	}  
+
+	/** Create a Thread[] array as large as the number of processors available. 
+	 * 	From Stephan Preibisch's Multithreading.java class. See: 
+	 * 	http://repo.or.cz/w/trakem2.git?a=blob;f=mpi/fruitfly/general/MultiThreading.java;hb=HEAD 
+	*/  
+	private Thread[] newThreadArray() {  
+		int n_cpus = Runtime.getRuntime().availableProcessors();  
+		IJ.log("CPUs:  "+n_cpus);
+		return new Thread[n_cpus];  
+	}  
+
+	/** Start all given threads and wait on each of them until all are done. 
+	 * From Stephan Preibisch's Multithreading.java class. See: 
+	 * http://repo.or.cz/w/trakem2.git?a=blob;f=mpi/fruitfly/general/MultiThreading.java;hb=HEAD 
+	 */  
+	public static void startAndJoin(Thread[] threads)  
+	{  
+		for (int ithread = 0; ithread < threads.length; ++ithread)  
+		{  
+			threads[ithread].setPriority(Thread.NORM_PRIORITY);  
+			threads[ithread].start();  
+		}  
+
+		try  
+		{     
+			for (int ithread = 0; ithread < threads.length; ++ithread)  
+				threads[ithread].join();  
+		} 
+		catch (InterruptedException ie)  
+		{  
+			throw new RuntimeException(ie);  
+		}  
+	}  
+}  
